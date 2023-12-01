5e28231261b5f775fcb53cd770d79ad1d76ae0e4
Simen Eilevstjønn <simene@lyse.net>
2023-12-01 21:18:08 +0100

    Add back one set of parantheses

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,2 +1,2 @@
 n="ø|one|two|three|four|five|six|seven|eight|nine"
-p $<.map{|l|m=l.scan(eval "/(?=(\\d|#{n}))/").map{|y|x=y[0];x=~/\d/?x.to_i: n.split(?|).index x};m[0]*10+m[-1]}.sum
+p $<.map{|l|m=l.scan(eval "/(?=(\\d|#{n}))/").map{|y|x=y[0];x=~/\d/?x.to_i: n.split(?|).index(x)};m[0]*10+m[-1]}.sum
