f8aaf86f29011dcb648b526b3bbff6f39f917a5f
Simen Eilevstjønn <simene@lyse.net>
2023-12-01 21:04:33 +0100

    Replace array with string

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,3 +1,3 @@
-n=[0,"one","two","three","four","five","six","seven","eight","nine"]
-v=->(x){return x=~/\d/?x.to_i: n.index x}
-p $<.map{|l|m=l.scan(eval("/(?=(\\d|"+n.join(?|)+"))/"));v.call(m[0][0])*10+v.call(m[-1][0])}.sum
+n="ø|one|two|three|four|five|six|seven|eight|nine"
+v=->(x){return x=~/\d/?x.to_i: n.split(?|).index(x)}
+p $<.map{|l|m=l.scan(eval("/(?=(\\d|#{n}))/"));v.call(m[0][0])*10+v.call(m[-1][0])}.sum
