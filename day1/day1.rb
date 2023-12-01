355a082ad03dd81d1a8d85f51a499872a88c51de
Simen Eilevstjønn <simene@lyse.net>
2023-12-01 19:56:31 +0100

    shorten

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,3 +1,3 @@
 n=[0,"one","two","three","four","five","six","seven","eight","nine"]
-v=->(x){return x=~/\d/? x.to_i : n.index(x)}
-p $<.map{|l|m=l.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/);v.call(m[0][0])*10+v.call(m[-1][0])}.sum
+v=->(x){return x=~/\d/?x.to_i: n.index x}
+p $<.map{|l|m=l.scan(eval("/(?=(\\d|"+n.join(?|)+"))/"));v.call(m[0][0])*10+v.call(m[-1][0])}.sum
