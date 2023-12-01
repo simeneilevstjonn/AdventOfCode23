d0a3a26ec8ffd60120bb35459acac544ad9fc7b7
Simen Eilevstjønn <simene@lyse.net>
2023-12-01 21:07:02 +0100

    Put lambda into map

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,3 +1,2 @@
 n="ø|one|two|three|four|five|six|seven|eight|nine"
-v=->(x){return x=~/\d/?x.to_i: n.split(?|).index(x)}
-p $<.map{|l|m=l.scan(eval("/(?=(\\d|#{n}))/"));v.call(m[0][0])*10+v.call(m[-1][0])}.sum
+p $<.map{|l|m=l.scan(eval("/(?=(\\d|#{n}))/")).map{|y|x=y[0];x=~/\d/?x.to_i: n.split(?|).index(x)};m[0]*10+m[-1]}.sum
