9f0858b2e4455cc58d5df06c1c1e5de5979c3770
Simen Eilevstjønn <simene@lyse.net>
2023-12-01 22:31:13 +0100

    Remove extra sum

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,2 +1,2 @@
 n="ø|one|two|three|four|five|six|seven|eight|nine"
-p $<.map{|l|m=l.scan(eval "/(?=(\\d|#{n}))/").map{|y|x,a=y;x=~/\d/?x.to_i: n.split(?|).index(x)};m[0]*10+m[-1]}.sum
+p $<.sum{|l|m=l.scan(eval "/(?=(\\d|#{n}))/").map{|y|x,a=y;x=~/\d/?x.to_i: n.split(?|).index(x)};m[0]*10+m[-1]}
