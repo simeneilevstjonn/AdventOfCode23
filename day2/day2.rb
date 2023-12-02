fd450ecbf4eddc8b2fce1a003614794671a5aaf3
Simen Eilevstjønn <simene@lyse.net>
2023-12-02 15:33:13 +0100

    Shorten a bit by using max at end

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,1 +1,1 @@
-p $<.sum{|l|r,g,b=0,0,0;a,c=l.split ": ";c.split(/,|;/).map{|x|n,t=x.split;n=n.to_i;(t<?c)?(b=[b,n].max):(t<?h)?(g=[g,n].max):(r=[r,n].max)};r*g*b}
+p $<.sum{|l|r,g,b=[],[],[];a,c=l.split ": ";c.split(/,|;/).map{|x|n,t=x.split;n=[n.to_i];(t<?c)?(b+=n):(t<?h)?(g+=n):(r+=n)};r.max*g.max*b.max}
