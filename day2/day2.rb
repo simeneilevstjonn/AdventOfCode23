21f789672f3bcb04214bad7014cb3b9d2f866c79
Simen Eilevstjønn <simene@lyse.net>
2023-12-02 16:02:04 +0100

    Change spaceship

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,1 +1,1 @@
-p $<.sum{|l|v=[[],[],[]];l.scan(/\d+ \w/).map{|x|n,t=x.split;v[(t<=>?c).+t<=>?h]+=[n.to_i]};v.map(&:max).inject :*}
+p $<.sum{|l|v=[[],[],[]];l.scan(/\d+ \w/).map{|x|n,t=x.split;v[t<=>?g]+=[n.to_i]};v.map(&:max).inject :*}
