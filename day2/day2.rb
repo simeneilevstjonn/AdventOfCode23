b92516b3879e0fb8b388508d3905fc8b71573dfe
Simen Eilevstjønn <simene@lyse.net>
2023-12-02 15:35:19 +0100

    Nested arrays

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,1 +1,1 @@
-p $<.sum{|l|r,g,b=[],[],[];a,c=l.split ": ";c.split(/,|;/).map{|x|n,t=x.split;n=[n.to_i];(t<?c)?(b+=n):(t<?h)?(g+=n):(r+=n)};r.max*g.max*b.max}
+p $<.sum{|l|v=[[],[],[]];a,c=l.split ": ";c.split(/,|;/).map{|x|n,t=x.split;v[(t<?c?0:1)+(t<?h?0:1)]+=[n.to_i]};v.map(&:max).inject(&:*)}
