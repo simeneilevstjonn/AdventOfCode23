69ad64c39f6455c83cd080f6458e4dc7fac3eed7
Simen Eilevstjønn <simene@lyse.net>
2023-12-02 15:42:38 +0100

    Remove unnescessary spaces

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,1 +1,1 @@
-p $<.sum{|l|v=[[],[],[]];a,c=l.split ": ";c.split(/,|;/).map{|x|n,t=x.split;v[(t<=>?c).+t<=>?h]+=[n.to_i]};v.map(&:max).inject(&:*)}
+p $<.sum{|l|v=[[],[],[]];a,c=l.split":";c.split(/,|;/).map{|x|n,t=x.split;v[(t<=>?c).+t<=>?h]+=[n.to_i]};v.map(&:max).inject(&:*)}
