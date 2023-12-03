c5b484ab3f34fd1e9e9a4a12f4caf46706d881fb
Simen Eilevstjønn <simene@lyse.net>
2023-12-03 13:18:24 +0100

    Start lambda

--- day3/day3_golf.py
+++ day3/day3_golf.py
@@ -1,5 +1,5 @@
 import re,sys
 g=[""]+sys.stdin.read().split()
 w=re.finditer
-q=lambda m,n:n[1]>=m[0]<m[1]>=n[0]
-print(sum(b[0]*b[1]for o,i in enumerate(g)for m in w("\*",i)for b in[[int(n.group(0))for d in[-1,0,1]for n in w("\d+",g[o+d])if q(m.span(),n.span())]]if len(b)==2))
+q=lambda w,x,y,z:z>=w<x>=y
+print(sum(b[0]*b[1]for o,i in enumerate(g)for m in w("\*",i)for b in[[int(n.group(0))for d in[-1,0,1]for n in w("\d+",g[o+d])if q(*m.span(),*n.span())]]if len(b)==2))
