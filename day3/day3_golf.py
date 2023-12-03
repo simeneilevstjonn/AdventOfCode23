ca769fc9d6f89384e66071d386575fd35c884e4d
Simen Eilevstjønn <simene@lyse.net>
2023-12-03 13:11:40 +0100

    Lambda instead of def

--- day3/day3_golf.py
+++ day3/day3_golf.py
@@ -1,5 +1,5 @@
 import re,sys
 g=[""]+sys.stdin.read().split()
 w=re.finditer
-def q(m,n):l,r=m.span();a,b=n.span();return b>=l<r>=a
-print(sum(b[0]*b[1]for o in range(len(g))for m in w("\*",g[o])for b in[[int(n.group(0))for d in[-1,0,1]for n in w("\d+",g[o+d])if q(m,n)]]if len(b)==2))
+q=lambda m,n:n[1]>=m[0]<m[1]>=n[0]
+print(sum(b[0]*b[1]for o in range(len(g))for m in w("\*",g[o])for b in[[int(n.group(0))for d in[-1,0,1]for n in w("\d+",g[o+d])if q(m.span(),n.span())]]if len(b)==2))
