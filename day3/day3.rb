3edbfc66d108e61b38363ff412e3c8a01af52394
Simen Eilevstjønn <simene@lyse.net>
2023-12-03 11:53:46 +0100

    Remove redundant parantheses

--- day3/day3.rb
+++ day3/day3.rb
@@ -3,4 +3,4 @@ g=0
 N=a.map{|l|i= ~g;m=!1;l.chars.map{g+=1 if m!=(m=_1=~/\d/)&&m;m ?[l.scan(/\d+/).map(&:to_i)[g+i],g+i]:0}}
 h=a.size-1
 w=a[0].size-2
-p (0..h).sum{|i|(0..w).sum{|j|r=[-1,0,1];n=r.product(r).map{|y,x|N[(i+y).clamp(0,h)][(j+x).clamp(0,w)]}.uniq-[0];n.size==2&&a[i][j]=='*'?(n.map{_1[0]}.inject(:*)):(0)}}
+p (0..h).sum{|i|(0..w).sum{|j|r=[-1,0,1];n=r.product(r).map{|y,x|N[(i+y).clamp(0,h)][(j+x).clamp(0,w)]}.uniq-[0];n.size==2&&a[i][j]=='*'?n.map{_1[0]}.inject(:*):0}}
