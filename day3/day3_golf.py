8a80eb0a12d9914b569e045d8e9528d5ebc2c677
Simen Eilevstjønn <simene@lyse.net>
2023-12-03 13:31:32 +0100

    Reintroduce *

--- day3/day3_golf.py
+++ day3/day3_golf.py
@@ -1,5 +1,5 @@
 import re,sys
 g=[""]+sys.stdin.read().split()
 w=re.finditer
-q=lambda x,y,z:z+2>x[1]>=y
-print(sum(b[0]*b[1]for o,i in enumerate(g)for m in w("\*",i)for b in[[int(n.group(0))for d in[-1,0,1]for n in w("\d+",g[o+d])if q(m.span(),*n.span())]]if len(b)==2))
+q=lambda w,x,y,z:z+2>x>=y
+print(sum(b[0]*b[1]for o,i in enumerate(g)for m in w("\*",i)for b in[[int(n.group(0))for d in[-1,0,1]for n in w("\d+",g[o+d])if q(*m.span(),*n.span())]]if len(b)==2))
