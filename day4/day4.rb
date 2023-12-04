d5f2a72ca87d8f70f84e884054d21c6aa249e9a0
Simen Eilevstjønn <simene@lyse.net>
2023-12-04 11:33:21 +0100

    Remove some local variables

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,5 +1,5 @@
 d=$<.map{_1}
 s=d.size
 n=[1]*s
-(0...s).map{|i|w=d[i].scan(/\d+(?=.*\|)/)[1..];c=d[i].split("| ")[1].split;c=(c&w).size;(i+1..[i+c,s-1].min).map{n[_1]+=n[i]}}
+(0...s).map{|i|(i+1..[i+(d[i].split("| ")[1].split&d[i].scan(/\d+(?=.*\|)/)[1..]).size,s-1].min).map{n[_1]+=n[i]}}
 p n.sum
