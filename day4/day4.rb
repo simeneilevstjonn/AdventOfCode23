3cd41326e61beae5ce282f9e41f65ca91951ac2f
Simen Eilevstjønn <simene@lyse.net>
2023-12-04 11:37:24 +0100

    Remove separate sum

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,5 +1,4 @@
 d=$<.map{_1}
 s=d.size
 n=[1]*s
-(0...s).map{|i|(i+1..[i+(d[i].split("| ")[1].split&d[i].scan(/\d+(?=.*\|)/)[1..]).size,s-1].min).map{n[_1]+=n[i]}}
-p n.sum
+p (0...s).sum{|i|(i+1..[i+(d[i].split("| ")[1].split&d[i].scan(/\d+(?=.*\|)/)[1..]).size,s-1].min).map{n[_1]+=n[i]};n[i]}
