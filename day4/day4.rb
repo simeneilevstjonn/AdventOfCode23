1d67780f4f20ae721dfa7fb497d116fabc42b177
Simen Eilevstjønn <simene@lyse.net>
2023-12-04 11:51:35 +0100

    Splat

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,3 +1,3 @@
-d=$<.map{[_1,1]}
+d=$<.map{[*_1.split(?|),1]}
 s=d.size
-p (0...s).sum{|i|x,y=d[i];a,b=x.split ?|;(i+1..[i+(b.split&a.scan(/\d+/)[1..]).size,s-1].min).map{d[_1][1]+=y};y}
+p (0...s).sum{|i|a,b,y=d[i];(i+1..[i+(b.split&a.scan(/\d+/)[1..]).size,s-1].min).map{d[_1][2]+=y};y}
