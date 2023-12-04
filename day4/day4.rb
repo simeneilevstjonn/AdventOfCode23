65984d7666b5f941157204a9100bdc72c9a46120
Simen Eilevstjønn <simene@lyse.net>
2023-12-04 11:53:01 +0100

    Move -1

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,3 +1,3 @@
 d=$<.map{[*_1.split(?|),1]}
-s=d.size
-p (0...s).sum{|i|a,b,y=d[i];(i+1..[i+(b.split&a.scan(/\d+/)[1..]).size,s-1].min).map{d[_1][2]+=y};y}
+s=d.size-1
+p (0..s).sum{|i|a,b,y=d[i];(i+1..[i+(b.split&a.scan(/\d+/)[1..]).size,s].min).map{d[_1][2]+=y};y}
