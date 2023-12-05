d7b86234242ef462b07b0d19cd206103717f8456
Simen Eilevstjønn <simene@lyse.net>
2023-12-05 18:37:38 +0100

    move to inside each_slice

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,32 +1,26 @@
 S,*M=`cat`.split("\n\n").map{_1.scan(/\d+/).map(&:to_i)}
 r=S.each_slice(2).to_a
-M.map{|m|r=r.flat_map{|a|
-s=[a]
-d=[]
+M.map{|m|r=r.flat_map{|a|s=[a];d=[];m.each_slice(3){|t,c,g|n = []
+for l, le in s do
+    h = le + l
 
-for dest, src, length in m.each_slice(3) do
-    n = []
-    for l, le in s do
-        h = le + l
-
-        if (src <= l && l < length + src) and (src <= h - 1 && h - 1< length + src)
-            d<<[l + dest - src, le]
-        elsif (src <= l && l< length + src)
-            d<<[l + dest - src, length + src - l]
-            if h - length - src > 0
-                n<<[length + src, h - length - src]
-            end
-        elsif (src <= h && h< length + src)
-            d<<[dest, h - src]
-            if src - l > 0
-                n<<[l, src - l]
-            end
-        else
-            n<<[l, le]
+    if (c <= l && l < g + c) and (c <= h - 1 && h - 1< g + c)
+        d<<[l + t - c, le]
+    elsif (c <= l && l< g + c)
+        d<<[l + t - c, g + c - l]
+        if h - g - c > 0
+            n<<[g + c, h - g - c]
         end
+    elsif (c <= h && h< g + c)
+        d<<[t, h - c]
+        if c - l > 0
+            n<<[l, c - l]
+        end
+    else
+        n<<[l, le]
     end
-    s = n
 end
+s = n}
 
 s + d
 }}
