b886769ed78f4f0e67aceb05afd26c58c2d20c59
Simen Eilevstjønn <simene@lyse.net>
2023-12-05 18:33:34 +0100

    Rename some long variables

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,33 +1,33 @@
-s,*M=`cat`.split("\n\n").map{_1.scan(/\d+/).map(&:to_i)}
-r=s.each_slice(2).to_a
+S,*M=`cat`.split("\n\n").map{_1.scan(/\d+/).map(&:to_i)}
+r=S.each_slice(2).to_a
 M.map{|m|r=r.flat_map{|a|
-srcranges = [a]
-destranges = []
+s=[a]
+d=[]
 
 for dest, src, length in m.each_slice(3) do
-    nsr = []
-    for l, le in srcranges do
+    n = []
+    for l, le in s do
         h = le + l
 
         if (src <= l && l < length + src) and (src <= h - 1 && h - 1< length + src)
-            destranges.append([l + dest - src, le])
+            d<<[l + dest - src, le]
         elsif (src <= l && l< length + src)
-            destranges.append([l + dest - src, length + src - l])
+            d<<[l + dest - src, length + src - l]
             if h - length - src > 0
-                nsr.append([length + src, h - length - src])
+                n<<[length + src, h - length - src]
             end
         elsif (src <= h && h< length + src)
-            destranges.append([dest, h - src])
+            d<<[dest, h - src]
             if src - l > 0
-                nsr.append([l, src - l])
+                n<<[l, src - l]
             end
         else
-            nsr.append([l, le])
+            n<<[l, le]
         end
     end
-    srcranges = nsr
+    s = n
 end
 
-srcranges + destranges
+s + d
 }}
 p r.min[0]
