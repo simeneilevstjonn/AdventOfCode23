a32c7be79139bf6fb220369f383441385958a078
Simen Eilevstjønn <simene@lyse.net>
2023-12-07 21:29:25 +0100

    Brute force day 7 part 2

--- day7/day7_part2.py
+++ day7/day7_part2.py
@@ -1,4 +1,4 @@
-data = open("day7/exdata7.txt").read().strip().split("\n")
+data = open("day7/data7.txt").read().strip().split("\n")
 
 cardorder = "AKQT98765432J"[::-1]
 
@@ -7,15 +7,10 @@ class Hand:
         self.hand, bid = row.split()
         self.bid = int(bid)
 
-    def type(self):
+    def _type(self, hypval):
         occ = {}
 
-        j = 0
-
-        for c in self.hand:
-            if c == "J":
-                j += 1
-                continue
+        for c in hypval:
             if c not in occ:
                 occ[c] = 1
             else:
@@ -23,66 +18,55 @@ class Hand:
 
         cnts = sorted(list(occ.values()))
 
-        if not j:
-            if len(occ) == 1:
-                # 5
-                # Five of a kind
-                return 6
-            
-            elif len(occ) == 2:
-                # 1, 4 or 2, 3
-                if cnts[1] == 4:
-                    # Four of a kind
-                    return 5
-                elif cnts[0] == 2:
-                    # Full House
-                    return 4
-            
-            elif len(occ) == 3:
-                # 1, 1, 3 or 1, 2, 2
-                if cnts[2] == 3:
-                    # Three of a kind
-                    return 3
-                if cnts[1] == 2 and cnts[2] == 2:
-                    # Two pairs
-                    return 2
-            
-            elif len(occ) == 4:
-                # 1, 1, 1, 2
-                # Pair
-                return 1
-            
-            else:
-                # 1, 1, 1, 1, 1
-                # High card
-                return 0
+        if len(occ) == 1:
+            # 5
+            # Five of a kind
+            return 6
         
-        else:
-            if len(occ) == 1:
-                # 1, 4j or 2, 3j or 3, 2j or 4, 1j
-                # Five of a kind
-                return 6
-            elif len(occ) == 2:
-                # 1, 1, 3j or 1, 2, 2j or 2, 2, 1j
+        elif len(occ) == 2:
+            # 1, 4 or 2, 3
+            if cnts[1] == 4:
                 # Four of a kind
-                if j == 3 or j == 2:
-                    return 5
-                # Full house
-                elif j == 1:
-                    return 4
-            elif len(occ) == 3:
-                # 1, 1, 1, 2j or 1, 1, 2, 1j
+                return 5
+            elif cnts[0] == 2:
+                # Full House
+                return 4
+        
+        elif len(occ) == 3:
+            # 1, 1, 3 or 1, 2, 2
+            if cnts[2] == 3:
                 # Three of a kind
                 return 3
-            elif len(occ) == 4:
-                # 1, 1, 1, 1, j
-                # One pair
-                return 1
-            elif len(occ) == 0:
-                # 5j
-                # Five of a kind
-                return 6
+            if cnts[1] == 2 and cnts[2] == 2:
+                # Two pairs
+                return 2
         
+        elif len(occ) == 4:
+            # 1, 1, 1, 2
+            # Pair
+            return 1
+        
+        else:
+            # 1, 1, 1, 1, 1
+            # High card
+            return 0
+    
+    def rangeOrSingleLetter(self, idx):
+        if self.hand[idx] != "J":
+            return [self.hand[idx]]
+        return cardorder[1:]
+        
+    def type(self):
+        mval = 0
+        for a in self.rangeOrSingleLetter(0):
+            for b in self.rangeOrSingleLetter(1):
+                for c in self.rangeOrSingleLetter(2):
+                    for d in self.rangeOrSingleLetter(3):
+                        for e in self.rangeOrSingleLetter(4):
+                            mval = max(mval, self._type("".join([a,b,c,d,e])))
+        return mval
+
+
     def sortstring(self):
         handstr = [chr(0x30 + cardorder.index(x)) for x in self.hand]
 
@@ -90,7 +74,6 @@ class Hand:
     
 hands = [Hand(r) for r in data]
 hands.sort(key = lambda x : x.sortstring())
-print(*[h.hand for h in hands])
 
 
 print(sum(h.bid * (i + 1) for i, h in enumerate(hands)))
