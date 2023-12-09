1971e3b90fb07866c74346297b4d88ca66b88109
Simen Eilevstjønn <simene@lyse.net>
2023-12-09 19:35:11 +0100

    Day 9 part 2

---
+++ day9/day9_part2.py
@@ -0,0 +1,31 @@
+data = open("day9/data9.txt").read().strip().split("\n")
+
+def diffArray(arr): 
+    return [arr[i] - arr[i - 1] for i in range(1, len(arr))]
+    
+def allZeroes(arr):
+    for i in arr:
+        if i != 0:
+            return False
+    return True
+
+def extrapolate(arr):
+    if allZeroes(arr):
+        # Nextdiff
+        return 0
+    
+    diffs = diffArray(arr)
+
+    nextdiff = extrapolate(diffs)
+    # print("Input array ", arr, "computed nextdiff", nextdiff, "cumdiff was", cumdiff, "retdiff is", arr[-1] + nextdiff)
+
+    return arr[0] - nextdiff
+
+sumcumdiffs = 0
+for seq in data:
+    a = [int(i) for i in seq.split()]
+    nextdiff = extrapolate(a)
+
+    sumcumdiffs += nextdiff
+
+print(sumcumdiffs)
