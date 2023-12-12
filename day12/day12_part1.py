131f6ed9c126238f101c967e4a90b723d7d32e08
Simen Eilevstjønn <simene@lyse.net>
2023-12-12 07:42:05 +0100

    Day 12 part 1 bruteforce

---
+++ day12/day12_part1.py
@@ -0,0 +1,28 @@
+data = open("day12/data12.txt").read().strip().split("\n")
+
+def valid(string, counts):
+    stspl = [len(i) for i in string.split(".") if i]
+    return counts == stspl
+
+summodes = 0
+for row in data:
+    string, counts = row.split()
+
+    counts = [int(i) for i in counts.split(",")]
+
+    smut = [i for i in string]
+
+    qmarkpos = []
+
+    for i, x in enumerate(smut):
+        if x == "?":
+            qmarkpos.append(i)
+
+    for i in range(1 << len(smut)):
+        for j, idx in enumerate(qmarkpos):
+            if i & (1 << j):
+                smut[idx] = "#"
+            else: smut[idx] = "."
+        summodes += valid("".join(smut), counts)
+
+print(summodes)
