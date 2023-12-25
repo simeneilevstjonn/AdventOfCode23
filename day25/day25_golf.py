c9391bc80af5f6fd76c78070b80b5bae4a9d5999
Simen Eilevstjønn <simene@lyse.net>
2023-12-25 22:43:58 +0100

    Make list comprehension

--- day25/day25_golf.py
+++ day25/day25_golf.py
@@ -1,7 +1,6 @@
 import networkx as n
 G=n.Graph()
-for r in open("d"):
- for d in r.split()[1:]:G.add_edge(r.split()[0][:3],d)
+[[G.add_edge(r.split()[0][:3],d)for d in r.split()[1:]]for r in open("d")]
 G.remove_edges_from(n.minimum_edge_cut(G))
 a,b=map(len,n.connected_components(G))
 print(a*b)
