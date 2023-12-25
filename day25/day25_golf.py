9e59a0861ef960addc97525cc2cd0e84d65bb54f
Simen Eilevstjønn <simene@lyse.net>
2023-12-25 22:54:34 +0100

    Wildcard import

--- day25/day25_golf.py
+++ day25/day25_golf.py
@@ -1,6 +1,6 @@
-import networkx as n
-G=n.Graph()
+from networkx import*
+G=Graph()
 [[G.add_edge(r.split()[0][:3],d)for d in r.split()[1:]]for r in open("d")]
-G.remove_edges_from(n.minimum_edge_cut(G))
-a,b=map(len,n.connected_components(G))
+G.remove_edges_from(minimum_edge_cut(G))
+a,b=map(len,connected_components(G))
 print(a*b)
