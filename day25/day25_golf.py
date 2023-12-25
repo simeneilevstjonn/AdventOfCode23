b0dc16a27a9036b85e10ab98588eb97b85ef90db
Simen Eilevstjønn <simene@lyse.net>
2023-12-25 22:37:18 +0100

    Remove newlines

--- day25/day25_golf.py
+++ day25/day25_golf.py
@@ -2,10 +2,8 @@ import networkx as n
 G=n.Graph()
 for r in open("d"):
  s,D=r.split(": ")
- for d in D.split():
-  G.add_edge(s,d)
+ for d in D.split():G.add_edge(s,d)
 G.remove_edges_from(n.minimum_edge_cut(G))
 a=1
-for s in n.connected_components(G):
- a*=len(s)
+for s in n.connected_components(G):a*=len(s)
 print(a)
