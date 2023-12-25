a9a0189621951d7b9c3bc0c24c791d8ebd40d25b
Simen Eilevstjønn <simene@lyse.net>
2023-12-25 22:34:39 +0100

    remove cut for

--- day25/day25_golf.py
+++ day25/day25_golf.py
@@ -4,8 +4,7 @@ for r in open("d"):
  s,D=r.split(": ")
  for d in D.split():
   G.add_edge(s,d)
-for u,v in n.minimum_edge_cut(G):
- G.remove_edge(u,v)
+G.remove_edges_from(n.minimum_edge_cut(G))
 a=1
 for s in n.connected_components(G):
  a*=len(s)
