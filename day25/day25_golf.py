37f11340b8bc8e8aa88854bd29a34969bbcbc5a0
Simen Eilevstjønn <simene@lyse.net>
2023-12-25 22:33:01 +0100

    shorten import alias

--- day25/day25_golf.py
+++ day25/day25_golf.py
@@ -1,12 +1,12 @@
-import networkx as nx
-G=nx.Graph()
+import networkx as n
+G=n.Graph()
 for row in open("d"):
  s,d=row.split(": ")
  for dest in d.split():
   G.add_edge(s,dest)
-for u,v in nx.minimum_edge_cut(G):
+for u,v in n.minimum_edge_cut(G):
  G.remove_edge(u,v)
 a=1
-for s in nx.connected_components(G):
+for s in n.connected_components(G):
  a*=len(s)
 print(a)
