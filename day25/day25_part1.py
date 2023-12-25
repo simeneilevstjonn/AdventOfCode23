f9ed24ed54cdad0475aa0b6a6ebc2b438ddbff39
Simen Eilevstjønn <simene@lyse.net>
2023-12-25 20:32:04 +0100

    Day 25 automatic with networkx

---
+++ day25/day25_part1.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+data = open("day25/data25.txt").read().strip().split("\n")
+
+sources = []
+dests = []
+
+for row in data:
+    s, d = row.split(": ")
+
+    for dest in d.split():
+        sources.append(s)
+        dests.append(dest)
+
+G = nx.Graph()
+
+G.add_edges_from(zip(sources, dests))
+
+for u, v in nx.minimum_edge_cut(G):
+    G.remove_edge(u, v)
+
+
+subgraphs = list(nx.connected_components(G))
+print(len(subgraphs[0]) * len(subgraphs[1]))
